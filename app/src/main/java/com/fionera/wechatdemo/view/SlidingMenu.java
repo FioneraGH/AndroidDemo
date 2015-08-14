545c555563fc464faeaf069fa023ee359c798c67
FioneraGH <fionera@outlook.com>
2015-08-14 21:17:21 +0800

    add custom attrs to SlidingMenu and add a button to toggle menu

--- app/src/main/java/com/fionera/wechatdemo/view/SlidingMenu.java
+++ app/src/main/java/com/fionera/wechatdemo/view/SlidingMenu.java
@@ -1,6 +1,7 @@
 package com.fionera.wechatdemo.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
@@ -10,6 +11,8 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+
+import com.fionera.wechatdemo.R;
 
 /**
  * Created by fionera on 15-8-14.
@@ -27,6 +30,12 @@ public class SlidingMenu extends HorizontalScrollView {
 
     private int mMenuWidth;
 
+    private boolean isOpen;
+
+    public SlidingMenu(Context context) {
+        this(context, null);
+    }
+
     /**
      * 未使用自定义属性时，调用
      *
@@ -34,16 +43,36 @@ public class SlidingMenu extends HorizontalScrollView {
      * @param attrs
      */
     public SlidingMenu(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, -1);
+    }
+
+    public SlidingMenu(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SlidingMenu,
+                defStyle, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+
+            switch (attr) {
+                case R.styleable.SlidingMenu_rightPadding:
+                    mMenuRightPadding = a.getDimensionPixelSize(attr,
+                            (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150,
+                                    context.getResources().getDisplayMetrics()));
+                    break;
+            }
+        }
+        a.recycle();
 
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics outMetrics = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(outMetrics);
         mWidthScreen = outMetrics.widthPixels;
 
-        //把50dp转换为像素值
-        mMenuRightPadding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150,
-                context.getResources().getDisplayMetrics());
+        //把150dp转换为像素值
+//        mMenuRightPadding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150,
+//                context.getResources().getDisplayMetrics());
     }
 
     /**
@@ -70,6 +99,7 @@ public class SlidingMenu extends HorizontalScrollView {
 
     /**
      * 布局参数，通过设置偏移量将Menu隐藏
+     *
      * @param changed
      * @param l
      * @param t
@@ -81,7 +111,7 @@ public class SlidingMenu extends HorizontalScrollView {
 
         super.onLayout(changed, l, t, r, b);
 
-        if(changed) {
+        if (changed) {
             this.scrollTo(mMenuWidth, 0);
         }
     }
@@ -90,15 +120,17 @@ public class SlidingMenu extends HorizontalScrollView {
     public boolean onTouchEvent(MotionEvent ev) {
 
         int action = ev.getAction();
-        switch (action){
+        switch (action) {
             case MotionEvent.ACTION_UP:
 
                 //隐藏在左边的宽度
                 int x = getScrollX();
-                if(x >mMenuWidth/2){
-                    this.smoothScrollTo(mMenuWidth,0);
-                }else{
-                    this.smoothScrollTo(0,0);
+                if (x > mMenuWidth / 2) {
+                    this.smoothScrollTo(mMenuWidth, 0);
+                    isOpen = false;
+                } else {
+                    this.smoothScrollTo(0, 0);
+                    isOpen = true;
                 }
 
                 return true;
@@ -106,5 +138,27 @@ public class SlidingMenu extends HorizontalScrollView {
 
         //不能覆盖，会造成其他Action失效
         return super.onTouchEvent(ev);
+    }
+
+    public void OpenMenu(){
+        if(!isOpen){
+            this.smoothScrollTo(0, 0);
+            isOpen = true;
+        }
+    }
+
+    public void CloseMenu(){
+        if(isOpen){
+            this.smoothScrollTo(mMenuWidth, 0);
+            isOpen = false;
+        }
+    }
+
+    public void ToogleMenu(){
+        if(isOpen){
+            CloseMenu();
+        }else{
+            OpenMenu();
+        }
     }
 }
