aca6a69f5110ccd2ff6320b5c7fac20f4849a186
FioneraGH <fionera@outlook.com>
2015-11-25 14:30:24 +0800

    update shooter stage to make the danmu not mix

--- app/src/main/java/com/fionera/wechatdemo/util/ShooterStage.java
+++ app/src/main/java/com/fionera/wechatdemo/util/ShooterStage.java
@@ -11,20 +11,31 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
+import com.fionera.wechatdemo.R;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.RunnableFuture;
 
 /**
  * Created by fionera on 15-11-24.
  */
 public class ShooterStage {
 
+    /**
+     * 假定一个字占用的像素
+     */
+    public static final int CHAR_WIDTH = 100;
     private Context context;
 
+    private Set<Integer> existVerticalMargins;
     /**
      * 弹幕显示数量，弹幕总数量
      */
@@ -40,7 +51,6 @@ public class ShooterStage {
     private int stageHeight = 0;
 
     private Timer timer;
-    private boolean isPause;
     private ArrayList<ObjectAnimator> objectAnimators;
     private ArrayList<String> danmuList;
 
@@ -50,6 +60,7 @@ public class ShooterStage {
         this.danmuList = danmuList;
         this.danmuCount = danmuList.size();
         objectAnimators = new ArrayList<>();
+        existVerticalMargins = new HashSet<>();
     }
 
     /**
@@ -76,25 +87,34 @@ public class ShooterStage {
                     public void onGlobalLayout() {
                         stageX = rlStage.getX();
                         stageY = rlStage.getY();
-                        stageWidth = rlStage.getWidth();
-                        stageHeight = rlStage.getHeight();
+                        stageWidth = rlStage.getWidth() - rlStage.getPaddingLeft() - rlStage
+                                .getPaddingRight();
+                        stageHeight = rlStage.getHeight() - rlStage.getPaddingTop() - rlStage
+                                .getPaddingBottom();
                         rlStage.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                     }
                 });
+
+        /**
+         * 构造一个用于在主线程上运行的Runnable
+         */
+        final Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                addDanmu(rlStage);
+            }
+        };
+
         timer = new Timer();
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                ((Activity) context).runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        addDanmu(rlStage);
-                    }
-                });
+                ((Activity) context).runOnUiThread(run);
             }
         };
-        timer.schedule(timerTask, 300, 1000);
+        timer.schedule(timerTask, 500, 1000);
     }
+
 
     /**
      * 实际的弹幕发送调用
@@ -103,22 +123,51 @@ public class ShooterStage {
      */
     private void addDanmu(final RelativeLayout rlStage) {
 
-        Log.d("---------", stageWidth + " " + stageHeight);
         String currentString = danmuList.get(count % danmuCount);
         /**
          * 生成随机种子
          */
         Random random = new Random(System.currentTimeMillis());
-        int seedHeight = random.nextInt(15);
+        int seedHeight = random.nextInt(stageHeight / CHAR_WIDTH - 1);
         int seedSize = random.nextInt(5);
+
+        /**
+         * 弹幕TextView
+         */
         final TextView textView = new TextView(context);
         textView.setText(currentString);
         textView.setTextColor(0xffffffff);
         textView.setSingleLine();
-        textView.setTranslationY(stageY + seedHeight * stageHeight / 20);
         textView.setTextSize(22 - seedSize * 2);
         textView.setBackground(new ColorDrawable(0x66ff0000));
-        textView.setTag(false);
+
+        /**
+         * 设置弹幕距离顶部高度，可以使用setTransactionY替代
+         */
+        while (true) {
+            int verticalMargin = seedHeight * CHAR_WIDTH + 10;
+            if (!existVerticalMargins.contains(verticalMargin)) {
+                existVerticalMargins.add(verticalMargin);
+                LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT);
+                params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                params.topMargin = verticalMargin;
+                textView.setLayoutParams(params);
+                textView.setTag(R.id.danmu_vertical_margin, verticalMargin);
+                break;
+            } else {
+                /**
+                 * 当前同屏显示的弹幕数量已经到达最大行数，则放弃此次弹幕发送
+                 */
+                if (count == stageHeight / CHAR_WIDTH) {
+                    return;
+                }
+                seedHeight = random.nextInt(stageHeight / CHAR_WIDTH);
+            }
+        }
+        LayoutParams p = (LayoutParams) textView.getLayoutParams();
+        Log.d("-------",seedHeight + " " + p.topMargin);
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -136,7 +185,6 @@ public class ShooterStage {
                         resumeStage(rlStage);
                     }
                 }).show();
-                isPause = !isPause;
             }
         });
 
@@ -144,9 +192,9 @@ public class ShooterStage {
         /**
          * TextView移动动画
          */
-        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(textView, "translationX",
-                stageX + stageWidth, stageX - (currentString.length() * 100)).setDuration(
-                10000 - seedSize * 700);
+        final ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(textView, "translationX",
+                stageX + stageWidth, stageX - (currentString.length() * CHAR_WIDTH)).setDuration(
+                10000 - seedSize * 500);
         objectAnimator.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
@@ -156,7 +204,11 @@ public class ShooterStage {
             @Override
             public void onAnimationEnd(Animator animation) {
 
+                /**
+                 * 移除被移除的弹幕的动画和被占用的margin
+                 */
                 objectAnimators.remove(animation);
+                existVerticalMargins.remove(textView.getTag(R.id.danmu_vertical_margin));
                 rlStage.removeView(textView);
                 count--;
             }
@@ -171,8 +223,8 @@ public class ShooterStage {
 
             }
         });
-        objectAnimator.start();
         objectAnimators.add(objectAnimator);
+        objectAnimator.start();
         rlStage.addView(textView);
         count++;
     }
