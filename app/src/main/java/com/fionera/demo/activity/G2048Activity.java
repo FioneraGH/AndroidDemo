d5796adb3f07cf3247aac716aea683944306c6ba
FioneraGH <fionera@outlook.com>
2016-02-11 12:10:38 +0800

    add a gallery made by hori scroll

--- app/src/main/java/com/fionera/demo/activity/G2048Activity.java
+++ app/src/main/java/com/fionera/demo/activity/G2048Activity.java
@@ -1,16 +1,60 @@
 package com.fionera.demo.activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
 
 import com.fionera.demo.R;
+import com.fionera.demo.util.DisplayUtils;
 
 public class G2048Activity
         extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_g2048);
+
+        linearLayout = (LinearLayout) findViewById(R.id.ll_image_container);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(DisplayUtils.dp2px(240),
+                                                                         DisplayUtils.dp2px(200));
+
+        ImageView imageView1 = new ImageView(this);
+        imageView1.setImageResource(R.drawable.ic_chat_head);
+        imageView1.setScaleType(ScaleType.CENTER);
+        imageView1.setBackgroundColor(Color.YELLOW);
+        linearLayout.addView(imageView1, params);
+
+        ImageView imageView2 = new ImageView(this);
+        imageView2.setImageResource(R.drawable.ic_chat_head);
+        imageView2.setScaleType(ScaleType.CENTER);
+        imageView2.setBackgroundColor(Color.RED);
+        linearLayout.addView(imageView2, params);
+
+        ImageView imageView3 = new ImageView(this);
+        imageView3.setImageResource(R.drawable.ic_chat_head);
+        imageView3.setScaleType(ScaleType.CENTER);
+        imageView3.setBackgroundColor(Color.GRAY);
+        linearLayout.addView(imageView3, params);
+
+
+        ImageView imageView4 = new ImageView(this);
+        imageView4.setImageResource(R.drawable.ic_chat_head);
+        imageView4.setScaleType(ScaleType.CENTER);
+        imageView4.setBackgroundColor(Color.BLUE);
+        linearLayout.addView(imageView4, params);
+
+
+        ImageView imageView5 = new ImageView(this);
+        imageView5.setImageResource(R.drawable.ic_chat_head);
+        imageView5.setScaleType(ScaleType.CENTER);
+        imageView5.setBackgroundColor(Color.GREEN);
+        linearLayout.addView(imageView5, params);
     }
 }
---
+++ app/src/main/java/com/fionera/demo/view/GalleryScrollView.java
@@ -0,0 +1,132 @@
+package com.fionera.demo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+/**
+ * Created by fionera on 16-2-11.
+ */
+public class GalleryScrollView
+        extends HorizontalScrollView {
+    private int subChildCount = 0;
+    private int downX = 0;
+    private int currentPage = 0;
+    private ArrayList<Integer> pointList = new ArrayList<>();
+
+    public GalleryScrollView(Context context) {
+        this(context, null);
+    }
+
+    public GalleryScrollView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GalleryScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+
+    private void init() {
+        setHorizontalScrollBarEnabled(false);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        receiveChildInfo();
+    }
+
+    public void receiveChildInfo() {
+
+        LinearLayout firstChild = (LinearLayout) getChildAt(0);
+        if (firstChild != null) {
+            subChildCount = firstChild.getChildCount();
+            for (int i = 0; i < subChildCount; i++) {
+                if (firstChild.getChildAt(i).getWidth() > 0) {
+                    if (0 != i) {
+                        pointList.add(firstChild.getChildAt(i).getLeft() - firstChild.getChildAt(i)
+                                .getWidth() / 4);
+                    }else{
+                        pointList.add(firstChild.getChildAt(i).getLeft());
+                    }
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = (int) ev.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL: {
+                if (Math.abs((ev.getX() - downX)) > getWidth() / 4) {
+                    if (ev.getX() - downX > 0) {
+                        smoothScrollToPrePage();
+                    } else {
+                        smoothScrollToNextPage();
+                    }
+                } else {
+                    smoothScrollToCurrent();
+                }
+                return true;
+            }
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    private void smoothScrollToCurrent() {
+        smoothScrollTo(pointList.get(currentPage), 0);
+    }
+
+    private void smoothScrollToNextPage() {
+        if (currentPage < subChildCount - 1) {
+            currentPage++;
+            smoothScrollTo(pointList.get(currentPage), 0);
+        }
+    }
+
+    private void smoothScrollToPrePage() {
+        if (currentPage > 0) {
+            currentPage--;
+            smoothScrollTo(pointList.get(currentPage), 0);
+        }
+    }
+
+    /**
+     * 上一页
+     */
+    public void prePage() {
+        smoothScrollToPrePage();
+    }
+
+    /**
+     * 下一页
+     */
+    public void nextPage() {
+        smoothScrollToNextPage();
+    }
+
+    /**
+     * 跳转到指定的页面
+     */
+    public boolean gotoPage(int position) {
+        if (position > 0 && position < subChildCount - 1) {
+            smoothScrollTo(pointList.get(position), 0);
+            currentPage = position;
+            return true;
+        }
+        return false;
+    }
+}
