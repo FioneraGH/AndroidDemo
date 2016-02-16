705aee694bed3b4e82b0c8079b4c27faf488a1e1
FioneraGH <fionera@outlook.com>
2016-02-16 23:05:02 +0800

    add kotlin support because of release 1.0 of it

---
+++ app/src/main/java/com/fionera/demo/activity/SmartTabLayoutActivity.kt
@@ -0,0 +1,62 @@
+package com.fionera.demo.activity
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+
+import com.fionera.demo.R
+import com.fionera.demo.fragment.TabLayoutFragment
+import com.fionera.demo.util.ShowToast
+import com.ogaclejapan.smarttablayout.SmartTabLayout
+
+class SmartTabLayoutActivity : AppCompatActivity() {
+
+    private var viewPager: ViewPager? = null
+    private var tabLayout: SmartTabLayout? = null
+
+    private val title = arrayOf("你好", "我并不好", "我真的很不好", "你", "你好", "我并不好", "我真的很不好", "你")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_smart_tab_layout)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        toolbar.inflateMenu(R.menu.menu_recycle)
+        toolbar.setOnMenuItemClickListener { item ->
+
+            ShowToast.show(item.title)
+            true
+        }
+        val mDrawerLayout = findViewById(R.id.drawer) as DrawerLayout
+        val mDrawerToggle = ActionBarDrawerToggle(this, mDrawerLayout,
+                toolbar, 0, 0)
+        mDrawerToggle.syncState()
+        mDrawerLayout.setDrawerListener(mDrawerToggle)
+
+        tabLayout = findViewById(R.id.tl_tab_layout) as SmartTabLayout
+        viewPager = findViewById(R.id.vp_tab_layout) as ViewPager
+
+        viewPager!!.adapter = object : FragmentPagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int): Fragment {
+
+                return TabLayoutFragment()
+            }
+
+            override fun getCount(): Int {
+                return title.size
+            }
+
+            override fun getPageTitle(position: Int): CharSequence {
+                return title[position % title.size]
+            }
+        }
+
+        tabLayout!!.setViewPager(viewPager)
+    }
+
+}
