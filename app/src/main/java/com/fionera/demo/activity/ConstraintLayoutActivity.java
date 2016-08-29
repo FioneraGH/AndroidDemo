3323c72e7b3dbe98e1ed2ad6cf8c3f973bdd445c
FioneraGH <fionera@outlook.com>
2016-08-29 10:35:40 +0800

    update api

--- app/src/main/java/com/fionera/demo/activity/ConstraintLayoutActivity.java
+++ app/src/main/java/com/fionera/demo/activity/ConstraintLayoutActivity.java
@@ -1,9 +1,12 @@
 package com.fionera.demo.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.fionera.demo.R;
+import com.fionera.demo.util.ShowToast;
 
 public class ConstraintLayoutActivity
         extends AppCompatActivity {
@@ -12,5 +15,13 @@ public class ConstraintLayoutActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout);
+    }
+
+    public void turnOnNight(View view) {
+        Intent localIntent = new Intent();
+        localIntent.setClassName("com.android.systemui", "com.android.systemui.tuner.TunerActivity");
+        localIntent.putExtra("show_night_mode", true);
+        startActivity(localIntent);
+        ShowToast.show("open");
     }
 }
