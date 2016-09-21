6b27abd2f1a076035ab81a6780df35178fef65fb
FioneraGH <fionera@outlook.com>
2016-09-21 13:47:28 +0800

    add ndk support and cmake list file

--- app/src/main/java/com/fionera/demo/activity/ConstraintLayoutActivity.java
+++ app/src/main/java/com/fionera/demo/activity/ConstraintLayoutActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.fionera.demo.R;
 import com.fionera.demo.util.ShowToast;
@@ -11,10 +12,21 @@ import com.fionera.demo.util.ShowToast;
 public class ConstraintLayoutActivity
         extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout);
+
+        textView= (TextView) findViewById(R.id.tv_constraint_tips);
+        textView.setText(stringFromJNI());
+    }
+
+    public native String stringFromJNI();
+
+    static {
+        System.loadLibrary("native-lib");
     }
 
     public void turnOnNight(View view) {
