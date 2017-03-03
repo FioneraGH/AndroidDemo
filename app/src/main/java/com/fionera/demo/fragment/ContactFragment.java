b87337730c4450412c509280d2ede960441c64c9
FioneraGH <fionera@outlook.com>
2017-03-03 11:03:53 +0800

    add constraint chaining demo

--- app/src/main/java/com/fionera/demo/fragment/ContactFragment.java
+++ app/src/main/java/com/fionera/demo/fragment/ContactFragment.java
@@ -2,6 +2,7 @@ package com.fionera.demo.fragment;
 
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
+import android.support.constraint.solver.widgets.ConstraintWidget;
 import android.support.transition.TransitionManager;
 import android.view.View;
 import android.widget.Button;
@@ -18,14 +19,10 @@ public class ContactFragment
         extends BaseFragment {
 
     private ConstraintLayout clContact;
-    private Button btnContactApply;
-    private Button btnContactReset;
-    private Button btnContact1;
-    private Button btnContact2;
-    private Button btnContact3;
 
     private ConstraintSet applyConstraintSet = new ConstraintSet();
     private ConstraintSet resetConstraintSet = new ConstraintSet();
+    private ConstraintSet chainConstraintSet = new ConstraintSet();
 
     @Override
     public int setLayoutResource() {
@@ -38,11 +35,12 @@ public class ContactFragment
 
         applyConstraintSet.clone(clContact);
         resetConstraintSet.clone(clContact);
+        chainConstraintSet.clone(clContact);
     }
 
     private void initView(View rootView) {
         clContact = (ConstraintLayout) rootView.findViewById(R.id.cl_contact);
-        btnContactApply = (Button) rootView.findViewById(R.id.btn_contact_apply);
+        Button btnContactApply = (Button) rootView.findViewById(R.id.btn_contact_apply);
         btnContactApply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,29 +64,71 @@ public class ContactFragment
 
                 applyConstraintSet.clear(R.id.btn_contact1);
 
+                applyConstraintSet.constrainHeight(R.id.btn_contact1, ConstraintSet.WRAP_CONTENT);
                 applyConstraintSet.connect(R.id.btn_contact1, ConstraintSet.START, R.id.cl_contact,
                         ConstraintSet.START, 0);
                 applyConstraintSet.connect(R.id.btn_contact1, ConstraintSet.END, R.id.cl_contact,
                         ConstraintSet.END, 0);
-                applyConstraintSet.connect(R.id.btn_contact1, ConstraintSet.TOP, R.id.cl_contact,
-                        ConstraintSet.TOP, 0);
-                applyConstraintSet.connect(R.id.btn_contact1, ConstraintSet.BOTTOM, R.id.cl_contact,
-                        ConstraintSet.BOTTOM, 0);
 
-                applyConstraintSet.setVisibility(R.id.btn_contact1, View.GONE);
+                applyConstraintSet.setVisibility(R.id.btn_contact4, View.GONE);
 
                 applyConstraintSet.applyTo(clContact);
             }
         });
-        btnContactReset = (Button) rootView.findViewById(R.id.btn_contact_reset);
+        Button btnContactChain = (Button) rootView.findViewById(R.id.btn_contact_chain);
+        btnContactChain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(clContact);
+
+                chainConstraintSet.clear(R.id.btn_contact1);
+                chainConstraintSet.clear(R.id.btn_contact2);
+                chainConstraintSet.clear(R.id.btn_contact3);
+                chainConstraintSet.clear(R.id.btn_contact4);
+
+                chainConstraintSet.constrainWidth(R.id.btn_contact1, ConstraintSet.WRAP_CONTENT);
+                chainConstraintSet.constrainWidth(R.id.btn_contact2, ConstraintSet.WRAP_CONTENT);
+                chainConstraintSet.constrainWidth(R.id.btn_contact3, ConstraintSet.WRAP_CONTENT);
+                chainConstraintSet.constrainWidth(R.id.btn_contact4, ConstraintSet.WRAP_CONTENT);
+
+                chainConstraintSet.constrainHeight(R.id.btn_contact1, ConstraintSet.WRAP_CONTENT);
+                chainConstraintSet.constrainHeight(R.id.btn_contact2, ConstraintSet.WRAP_CONTENT);
+                chainConstraintSet.constrainHeight(R.id.btn_contact3, ConstraintSet.WRAP_CONTENT);
+                chainConstraintSet.constrainHeight(R.id.btn_contact4, ConstraintSet.WRAP_CONTENT);
+
+                chainConstraintSet.connect(R.id.btn_contact1, ConstraintSet.START, R.id.cl_contact,
+                        ConstraintSet.START, 0);
+                chainConstraintSet.connect(R.id.btn_contact4, ConstraintSet.END, R.id.cl_contact,
+                        ConstraintSet.END, 0);
+                chainConstraintSet.connect(R.id.btn_contact2, ConstraintSet.START,
+                        R.id.btn_contact1, ConstraintSet.END, 0);
+                chainConstraintSet.connect(R.id.btn_contact1, ConstraintSet.END, R.id.btn_contact2,
+                        ConstraintSet.START, 0);
+                chainConstraintSet.connect(R.id.btn_contact3, ConstraintSet.START,
+                        R.id.btn_contact2, ConstraintSet.END, 0);
+                chainConstraintSet.connect(R.id.btn_contact2, ConstraintSet.END, R.id.btn_contact3,
+                        ConstraintSet.START, 0);
+                chainConstraintSet.connect(R.id.btn_contact4, ConstraintSet.START,
+                        R.id.btn_contact3, ConstraintSet.END, 0);
+                chainConstraintSet.connect(R.id.btn_contact3, ConstraintSet.END, R.id.btn_contact4,
+                        ConstraintSet.START, 0);
+
+                chainConstraintSet.createHorizontalChainRtl(R.id.btn_contact1, ConstraintSet.START,
+                        R.id.btn_contact4, ConstraintSet.END,
+                        new int[]{R.id.btn_contact1, R.id.btn_contact4}, null,
+                        ConstraintWidget.CHAIN_SPREAD);
+
+                chainConstraintSet.applyTo(clContact);
+            }
+        });
+        Button btnContactReset = (Button) rootView.findViewById(R.id.btn_contact_reset);
         btnContactReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(clContact);
+
                 resetConstraintSet.applyTo(clContact);
             }
         });
-        btnContact1 = (Button) rootView.findViewById(R.id.btn_contact1);
-        btnContact2 = (Button) rootView.findViewById(R.id.btn_contact2);
-        btnContact3 = (Button) rootView.findViewById(R.id.btn_contact3);
     }
 }
